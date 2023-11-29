1d13025d8eab4516af1966a781aa529031fa16bf
Lucas-dev-br <yulucasgx2@hotmail.com>
2023-11-29 08:40:34 -0300

    att

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import './Auth.css'
+import { Link } from 'react-router-dom';
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div id="login">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Faça o login para ver o que há de novo.</p>
+      <form >
+        <input
+          type="text"
+          placeholder="E-mail"
+  
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+         
+        />
+
+<input type="submit" value="Entrar" />
+       
+      </form>
+      <p>
+        Não tem uma conta? <Link to="/register">Clique aqui</Link>
+      </p>
+    </div>
+  );
 }
 
 export default Login
